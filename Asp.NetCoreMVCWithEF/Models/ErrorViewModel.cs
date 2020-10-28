c638893526d82c2f1a9b20901510dd3c1d85d807
Avdhoota Megha <avdhoota.megha@gmail.com>
2020-10-28 16:40:40 +0530

    Add project files.

---
+++ Asp.NetCoreMVCWithEF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Asp.NetCoreMVCWithEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
